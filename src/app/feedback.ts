6eb45bf77dfb8bb5d1ea2fe55fda5ce8639d3bf2
suryateja99 <34399135+suryateja99@users.noreply.github.com>
2022-08-02 16:24:29 -0400

    Added feedback model

---
+++ src/app/feedback.ts
@@ -0,0 +1,13 @@
+export class Feedback {
+
+    constructor(
+      public name: string,
+      public email: string,
+      public address: string,
+      public phone: string,
+      public occupation: string,
+      public preferredMethodOfContact: string,
+      public comments: string,
+    ) {  }
+  
+  }
